0c59377386b7fa166ea864027fc6b2c9948c6f52
王亮亮 <1695699447@qq.com>
2024-01-12 11:16:41 +0800

    feat:小程序码参数全部传入

--- src/library/QrCode.php
+++ src/library/QrCode.php
@@ -182,27 +182,21 @@ class  QrCode
      * Time: 9:52
      * Email:1695699447@qq.com
      * @param $access_token :第三方平台接口调用令牌authorizer_access_token
-     * @param $scene        :最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
-     * @param $page         :必须是已经发布的小程序存在的页面（否则报错），例如 pages/index/index, 根路径前不要填加 /,不能携带参数（参数请放在scene字段里），如果不填写这个字段，默认跳主页面
-     * @param $width        :二维码的宽度，单位 px，最小 280px，最大 1280px
-     * @param $auto_color   :自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调，默认 false
-     * @param $line_color   :auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示
-     * @param $is_hyaline   :是否需要透明底色，为 true 时，生成透明底色的小程序
+	 * @param $data :下列
+     * $scene        :最大32个可见字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~，其它字符请自行编码为合法字符（因不支持%，中文无法使用 urlencode 处理，请使用其他编码方式）
+     * $page         :必须是已经发布的小程序存在的页面（否则报错），例如 pages/index/index, 根路径前不要填加 /,不能携带参数（参数请放在scene字段里），如果不填写这个字段，默认跳主页面
+     * $width        :二维码的宽度，单位 px，最小 280px，最大 1280px
+     * $auto_color   :自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调，默认 false
+     * $line_color   :auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示
+     * $is_hyaline   :是否需要透明底色，为 true 时，生成透明底色的小程序
      * @return mixed
      */
-    public function getWxaCodeUnLimit($access_token, $scene, $page, $width, $auto_color, $line_color, $is_hyaline)
+    public function getWxaCodeUnLimit($access_token, $data)
     {
         return $this->curl->post(
             UrlConfig::getWxaCodeUnLimit . $access_token,
             json_encode(
-                [
-                    "scene" => $scene,
-                    'page' => $page,
-                    "width" => $width,
-                    'auto_color' => $auto_color,
-                    "line_color" => $line_color,
-                    'is_hyaline' => $is_hyaline,
-                ]
+				$data
             )
         );
     }
@@ -218,25 +212,20 @@ class  QrCode
      * Time: 9:54
      * Email:1695699447@qq.com
      * @param $access_token :第三方平台接口调用令牌authorizer_access_token
-     * @param $page         :扫码进入的小程序页面路径，最大长度 128 字节，不能为空；对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"，即可在 wx.getLaunchOptionsSync 接口中的 query 参数获取到 {foo:"bar"}。
-     * @param $width        :二维码的宽度，单位 px。最小 280px，最大 1280px
-     * @param $auto_color   :    自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
-     * @param $line_color   :auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示
-     * @param $is_hyaline   :是否需要透明底色，为 true 时，生成透明底色的小程序码
+	 * @param $data :下列
+     * $page         :扫码进入的小程序页面路径，最大长度 128 字节，不能为空；对于小游戏，可以只传入 query 部分，来实现传参效果，如：传入 "?foo=bar"，即可在 wx.getLaunchOptionsSync 接口中的 query 参数获取到 {foo:"bar"}。
+     * $width        :二维码的宽度，单位 px。最小 280px，最大 1280px
+     * $auto_color   :    自动配置线条颜色，如果颜色依然是黑色，则说明不建议配置主色调
+     * $line_color   :auto_color 为 false 时生效，使用 rgb 设置颜色 例如 {"r":"xxx","g":"xxx","b":"xxx"} 十进制表示
+     * $is_hyaline   :是否需要透明底色，为 true 时，生成透明底色的小程序码
      * @return mixed
      */
-    public function getWxaCode($access_token, $page, $width, $auto_color, $line_color, $is_hyaline)
+    public function getWxaCode($access_token, $data)
     {
         return $this->curl->post(
             UrlConfig::getWxaCode . $access_token,
             json_encode(
-                [
-                    'page' => $page,
-                    "width" => $width,
-                    'auto_color' => $auto_color,
-                    "line_color" => $line_color,
-                    'is_hyaline' => $is_hyaline,
-                ]
+				$data
             )
         );
     }
